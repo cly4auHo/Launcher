f6ce7fb6c2b6ec528a634ffc80af1519c8fc5b8c
cly4auHo <arsen.dmitriev96@gmail.com>
2019-09-11 23:26:36 +0300

    Last

--- Assets/Scripts/RocketLauncher.cs
+++ Assets/Scripts/RocketLauncher.cs
@@ -3,14 +3,17 @@
 public class RocketLauncher : MonoBehaviour
 {
     [SerializeField] private GameObject rocketPrefab;
-    public bool IsRocketLaunched => rocket;   
+    public bool IsRocketLaunched => rocket;
     private GameObject rocket;
 
     void Update()
     {
-        if (!rocket && Input.GetMouseButtonDown(0))
+        if (!rocket)
         {
-            rocket = GameObject.Instantiate(rocketPrefab, Vector3.zero, Quaternion.identity);
+            if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+            {
+                rocket = GameObject.Instantiate(rocketPrefab, Vector3.zero, Quaternion.identity);
+            }
         }
     }
 }
